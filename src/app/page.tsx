e0cf9e1eabd1117f9df33aad806c66d07b8db990
arochat9 <andybrochat@gmail.com>
2025-03-02 16:39:59 -0500

    updating code to use new schema

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,36 +2,19 @@ import { db } from "../server/db";
 
 export const dynamic = "force-dynamic";
 
-const mockURLs = [
-  "https://lfp7jezmxg.ufs.sh/f/V1haWO7ECfU99nhi4w7QjpQutiANWh9IH4D3EBOT0V8kX6aF",
-  "https://lfp7jezmxg.ufs.sh/f/V1haWO7ECfU9krwUN0FtLflbuysXz4K62j8w0QnN3r7CedPD",
-  "https://lfp7jezmxg.ufs.sh/f/V1haWO7ECfU9hsOrkpnr0UMg4SoCpLH9IewDRAPzNJFdBuZX",
-  "https://lfp7jezmxg.ufs.sh/f/V1haWO7ECfU9GS5oecVhnEPX1SpDOUTV80sYIRq2aFyiMmrW",
-  "https://lfp7jezmxg.ufs.sh/f/V1haWO7ECfU93nGB3BxYGp50NBcTgea7MbkfzFd6EIX8iUwl",
-  "https://lfp7jezmxg.ufs.sh/f/V1haWO7ECfU9TZf7Bqot5NbdiYZ91F7wcLE4qDSBvpa2HuWf",
-  "https://lfp7jezmxg.ufs.sh/f/V1haWO7ECfU9itNiEwjsZELWcfG2HdD6aXoylBgYtMqTvRK8",
-];
-
-const mockImages = [...mockURLs, ...mockURLs, ...mockURLs].map((url, i) => ({
-  id: i + 1,
-  url,
-}));
-
 export default async function HomePage() {
-  const posts = await db.query.posts.findMany();
-  console.log(posts);
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.createdAt),
+  });
+  console.log(images);
 
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {posts.map((post) => (
-          <div key={post.id} className="w-48">
-            {post.name}
-          </div>
-        ))}
-        {mockImages.map((image) => (
-          <div key={image.id} className="w-48">
-            <img src={image.url} alt={`Image ${image.id}`} />
+        {images.map((image) => (
+          <div key={image.id} className="flex w-48 flex-col">
+            <img src={image.url} alt={`Image ${image.name}`} />
+            <div>{image.name}</div>
           </div>
         ))}
       </div>
