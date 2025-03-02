3c7655e6957145763866ad80493b3dc07441dd53
arochat9 <andybrochat@gmail.com>
2025-03-02 17:17:22 -0500

    use auth for homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,35 @@
 import { asc } from "drizzle-orm";
 import { db } from "../server/db";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
+async function Images() {
   const images = await db.query.images.findMany({
     orderBy: (model, { desc }) => asc(model.createdAt),
   });
   console.log(images);
+  return (
+    <div className="flex flex-wrap gap-4">
+      {images.map((image) => (
+        <div key={image.id} className="flex w-48 flex-col">
+          <img src={image.url} alt={`Image ${image.name}`} />
+          <div>{image.name}</div>
+        </div>
+      ))}
+    </div>
+  );
+}
 
+export default async function HomePage() {
   return (
     <main className="">
-      <div className="flex flex-wrap gap-4">
-        {images.map((image) => (
-          <div key={image.id} className="flex w-48 flex-col">
-            <img src={image.url} alt={`Image ${image.name}`} />
-            <div>{image.name}</div>
-          </div>
-        ))}
-      </div>
+      <SignedOut>
+        <div className="h-full w-full text-center text-2xl">Please Sign In</div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
